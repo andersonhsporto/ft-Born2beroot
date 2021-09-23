dc0537465308456e3ab4343faac08355a0d67ed9
Anderson Porto <andersonhigo2@gmail.com>
2021-09-22 21:45:45 -0300

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -13,16 +13,17 @@ lvm_check=$(lsblk | grep "lvm" | wc -l)
 ipv4=$(ip route get 1.2.3.4 | awk 'NR ==1 {print $7}')
 macadd=$(ip address | grep "link/ether" | awk '{printf "(%s)", $2}')
 
-
-echo "#Architecture:" $(uname -a)
-echo "#CPU physical:" $(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
-echo "#vCPU:" $(grep "processor" /proc/cpuinfo | sort | uniq | wc -l)
-echo "#Memory Usage:" "$ram_used/$ram_total" "($ram_percent)"
-echo "#Disk Usage:" "$disk_used/$disk_total" "($disk_percent)"
-echo "#CPU load:" $(mpstat | awk 'NR == 4 {printf "%.1f%%", 100-$12}')
-echo "#Last boot:" $(who -b -t | awk '{print $4} {print $5}')
-echo "#LVM use:" $(if [ $lvm_check == 0 ]; then echo no; else echo yes; fi)
-echo "#Connexions TCP:" $(ss -s | awk '/TCP:/ {print $2}')
-echo "#User log:" $(who | wc -l)
-echo "#Network: IP" $ipv4 $macadd
-echo "#Sudo:" $(grep "COMMAND" /var/log/sudo/sudo.log | wc -l) "cmd"
+wall -n "\
+#Architecture: $(uname -a)
+#CPU physical: $(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l) 
+#vCPU: $(grep "processor" /proc/cpuinfo | sort | uniq | wc -l)
+#Memory Usage: $ram_used/$ram_total "$ram_percent"
+#Disk Usage: "$disk_used/$disk_total" "$disk_percent"
+#CPU load: $(mpstat | awk 'NR == 4 {printf "%.1f%%", 100-$13}')
+#Last boot: $(who -b| awk '$1 == "system" {print $3} {print $4}')
+#LVM use: $(if [ $lvm_check == 0 ]; then echo no; else echo yes; fi)
+#Connexions TCP: $(ss -s | awk '/TCP:/ {print $2}')
+#User log: $(who | wc -l)
+#Network: IP $ipv4 $macadd
+#Sudo: $(grep "COMMAND" /var/log/sudo/sudo.log | wc -l) "cmd"
+"
